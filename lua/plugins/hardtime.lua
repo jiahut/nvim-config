b7939291c2282417f03bed07175a1e7d3ed1df25
zhijia.zhang <jiahut@gmail.com>
2026-04-19 20:58:20 +0800

    优化 hard time 的配置，一秒可以按键10次

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -2,6 +2,7 @@ return {
   "m4xshen/hardtime.nvim",
   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
   opts = {
+    max_count = 10, -- 允许在 1000ms 内连续按键的最大次数 (默认是 3，这里改成 10)
     disabled_filetypes = { -- 在某些文件类型中禁用 (比如文件树)
       "NvimTree",
       "neo-tree",
